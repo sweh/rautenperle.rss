df5f895da67319e46b4a7f7f8a6551ce941355c2
Sebastian Wehrmann <sebastian.wehrmann@icloud.com>
2019-02-15 09:04:28 +0100

    Use local mercury parser

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
-from mercury_parser import ParserAPI
-import configparser
 import urllib.request
+import subprocess
+import json
 
 XML = """
 <?xml version="1.0" encoding="UTF-8" ?>
@@ -23,11 +23,11 @@ XML_ITEM = """
     </item>"""
 
 
+def get_json(url):
+    return json.loads(
+        subprocess.Popen(f"mercury-parser {url}", shell=True, stdout=subprocess.PIPE).stdout.read())
+
 def generate():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    MAPIKEY = config['mercury']['apikey']
-    mercury = ParserAPI(api_key=MAPIKEY)
     fp = urllib.request.urlopen("http://www.rautenperle.com")
     mybytes = fp.read()
     html = mybytes.decode("utf8")
@@ -35,18 +35,18 @@ def generate():
     soup = BeautifulSoup(html, 'html.parser')
     storyelem = soup.find('h1').find('a')
     url = 'http://www.rautenperle.com' + storyelem.attrs['href']
-    p = mercury.parse(url)
+    p = get_json(url)
     items = XML_ITEM.format(
         title=storyelem.find('span').contents[0].strip(),
         link='http://www.rautenperle.com' + storyelem.attrs['href'],
-        description=p.content)
+        description=p['content'])
     for item in soup.find_all('h2'):
         try:
             linkelem = item.find('a')
             title = linkelem.contents[0].strip()
             url = 'http://www.rautenperle.com' + linkelem.attrs['href']
-            p = mercury.parse(url)
-            items += XML_ITEM.format(title=title, link=url, description=p.content)
+            p = get_json(url)
+            items += XML_ITEM.format(title=title, link=url, description=p['content'])
         except Exception:
             continue
     print(XML.format(items=items))
