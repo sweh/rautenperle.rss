f5b6c1bc863c5bc634fafaf52da79beb0ec5e15b
Sebastian Wehrmann <sebastian.wehrmann@icloud.com>
2020-10-20 07:42:41 +0200

    Bugfixes for new RSS reader

--- generate.py
+++ generate.py
@@ -2,15 +2,17 @@ from bs4 import BeautifulSoup
 import urllib.request
 import subprocess
 import json
+import re
 
-XML = """
+XML = """\
 <?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 
   <channel>
     <title>rautenperle.com</title>
     <link>https://www.rautenperle.com</link>
     <description>Mein Schatz</description>
+    <atom:link href="https://www.wehrmann.it/rautenperle.xml" rel="self" type="application/rss+xml" />
     {items}
   </channel>
 </rss>"""
@@ -19,9 +21,12 @@ XML_ITEM = """
     <item>
       <title>{title}</title>
       <link>{link}</link>
+      <guid>{link}</guid>
+      <enclosure url="{lead_image_url}" length="2048" type="image/jpeg" />
       <description>{description}</description>
     </item>"""
 
+GUIDS = []
 
 def get_json(url):
     return json.loads(
@@ -36,17 +41,26 @@ def generate():
     storyelem = soup.find('h1').find('a')
     url = 'http://www.rautenperle.com' + storyelem.attrs['href']
     p = get_json(url)
+    GUIDS.append(url)
     items = XML_ITEM.format(
-        title=storyelem.find('span').contents[0].strip(),
+        title=storyelem.find('span').contents[0].strip().replace('&', 'und'),
         link='http://www.rautenperle.com' + storyelem.attrs['href'],
-        description=p['content'])
+        lead_image_url=p.get('lead_image_url', 'https://www.wehrmann.it/rautenperle.jpg'),
+        description=re.sub('<[^<]+?>', '', p['content']))
     for item in soup.find_all('h2'):
         try:
             linkelem = item.find('a')
-            title = linkelem.contents[0].strip()
+            title = linkelem.contents[0].strip().replace('&', 'und')
             url = 'http://www.rautenperle.com' + linkelem.attrs['href']
+            if url in GUIDS:
+                continue
+            GUIDS.append(url)
             p = get_json(url)
-            items += XML_ITEM.format(title=title, link=url, description=p['content'])
+            items += XML_ITEM.format(
+                title=title,
+                link=url,
+                lead_image_url=p.get('lead_image_url', 'https://www.wehrmann.it/rautenperle.jpg'),
+                description=re.sub('<[^<]+?>', '', p['content']))
         except Exception:
             continue
     print(XML.format(items=items))
---
+++ generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/sweh/rautenperle.rss/
+bin/python generate.py > ../wehrmann.it/rautenperle.xml
